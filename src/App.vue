d26f46a34bbc831e74f6c584f983b5dcb10d228d
q-wang <apeidou@gmail.com>
2018-09-15 20:51:46 +0800

    add coreui

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import '../node_modules/@coreui/icons/css/coreui-icons.min.css';
-
+$fa-font-path: '~font-awesome/fonts/';
+@import '~font-awesome/scss/font-awesome';
+$simple-line-font-path: '~simple-line-icons/fonts/';
+@import '~simple-line-icons/scss/simple-line-icons';
+@import '../node_modules/flag-icon-css/css/flag-icon.min.css';
+@import 'assets/scss/style';
 </style>
