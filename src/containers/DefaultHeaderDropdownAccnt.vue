1deb4bb91ab0a46488f3bff3438354ce59ee8736
q-wang <apeidou@gmail.com>
2018-09-16 14:44:30 +0800

    change img urls

--- src/containers/DefaultHeaderDropdownAccnt.vue
+++ src/containers/DefaultHeaderDropdownAccnt.vue
@@ -2,7 +2,7 @@
   <AppHeaderDropdown right no-caret>
     <template slot="header">
       <img
-        src="img/avatars/6.jpg"
+        src="static/img/avatars/6.jpg"
         class="img-avatar"
         alt="admin@bootstrapmaster.com" />
     </template>\
