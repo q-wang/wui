72e1d731793d670fa5ec885e32beb4393e631e74
q-wang <apeidou@gmail.com>
2018-09-22 18:24:21 +0800

    missing

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 export default new Router({
   mode: 'hash', // https://router.vuejs.org/api/#mode
   linkActiveClass: 'open active',
-  // scrollBehavior: () => ({ y: 0 }),
+  scrollBehavior: () => ({ y: 0 }),
   routes: [
     {
       path: '/',
